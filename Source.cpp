ec85b236d4182dfbb736182f7639b81af87bdde8
meliha dikmen <62018540+melihadikmen@users.noreply.github.com>
2020-04-16 23:55:32 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,109 @@
+#include <windows.h>
+#include <wchar.h>
+#include "pxcsensemanager.h"
+#include <opencv2/opencv.hpp>
+#include <iostream> 
+#include "util_render.h"
+#include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
+
+using namespace std;
+#define MAX_FRAMES 150
+int wmain(int argc, WCHAR* argv[]) {
+	// initialize the util render
+	UtilRender *renderDepth = new UtilRender(L"DEPTH STREAM");
+	// create the PXCSenseManager
+	PXCSenseManager *psm = 0;
+	psm = PXCSenseManager::CreateInstance();
+	if (!psm) {
+		wprintf_s(L"Unable to create the PXCSenseManager\n");
+		return 1;
+	}
+	// select the color stream of size 640x480 and depth stream of size 320x240
+	//psm->EnableStream(PXCCapture::STREAM_TYPE_COLOR, 640, 480);
+	psm->EnableStream(PXCCapture::STREAM_TYPE_DEPTH, 480, 360);
+	// initialize the PXCSenseManager
+	if (psm->Init() != PXC_STATUS_NO_ERROR) return 2;
+	PXCPoint3DF32 *pos3D = new PXCPoint3DF32[480 * 360];
+	for (int i = 0; i < MAX_FRAMES; i++) {
+		// This function blocks until all streams are ready (depth and color)
+		// if false streams will be unaligned
+		if (psm->AcquireFrame(true) < PXC_STATUS_NO_ERROR) break;
+		// retrieve all available image samples
+		PXCCapture::Sample *sample = psm->QuerySample();
+		PXCImage  *depthMap = sample->depth;
+		renderDepth->RenderFrame(sample->depth);
+		PXCImage::ImageData depthImage;
+		depthMap->AcquireAccess(PXCImage::ACCESS_READ, &depthImage);
+		PXCImage::ImageInfo imgInfo = depthMap->QueryInfo();
+		int depth_stride = depthImage.pitches[0] / sizeof(pxcU16);
+		PXCPoint3DF32 * pos3d = new PXCPoint3DF32[480 * 360];
+		PXCCaptureManager* captureManager = psm->QueryCaptureManager();
+		PXCCapture::Device* device = captureManager->QueryDevice();
+		PXCProjection* projection = device->CreateProjection();
+		pxcU16 *dpixels = (pxcU16*)depthImage.planes[0];
+		unsigned int dpitch = depthImage.pitches[0] / sizeof(pxcU16);
+		pxcStatus sts = psm->AcquireFrame(true);
+		sts = projection->QueryVertices(depthMap, &pos3D[0]);
+		if (sts < pxcStatus::PXC_STATUS_NO_ERROR) {
+			wprintf_s(L"Projection was unsuccessful! \n");
+			psm->Close();
+		}
+		
+		if (!renderDepth->RenderFrame(depthMap)) break;
+		// release or unlock the current frame to fetch the next frame
+		psm->ReleaseFrame();
+	}
+
+	for (int k = 0; k < 480 * 360; k++) {
+
+		if (pos3D[k].x != 0) {
+			cout << " xx is: " << pos3D[k].x << endl;
+		}
+
+		if (pos3D[k].y != 0) {
+
+			cout << " yy is: " << pos3D[k].y << endl;
+		}
+		if (pos3D[k].z != 0) {
+			cout << " zz is: " << pos3D[k].z << endl;
+		}
+		//Sleep(100);
+	}
+	pcl::PointCloud<pcl::PointXYZRGBNormal> cloud;
+	// Fill in the cloud data
+	cloud.width = 480;
+	cloud.height = 360;
+	cloud.is_dense = true;
+	cloud.points.resize(cloud.width * cloud.height);
+	cloud.sensor_orientation_ = Eigen::Quaternionf(0, 1, 0, 0);
+
+	int writeCount = 0;
+	for (std::size_t i = 0; i < cloud.points.size(); ++i)
+	{
+	cloud.points[i].x = pos3D[i].x;
+	cloud.points[i].y = pos3D[i].y;
+	cloud.points[i].z = pos3D[i].z;
+	cloud.points[i].normal[0] = 1.f;
+	cloud.points[i].normal[1] = 0.f;
+	cloud.points[i].normal[2] = 0.0f;
+	cloud.points[i].r = 255;
+	cloud.points[i].g = 125;
+	cloud.points[i].b = 0;
+	}
+	writeCount++;
+	//char filename[100];
+	//sprintf(filename, "C:/Users/Hp/Desktop/ornek.xyz", writeCount);
+	//pcl::io::savePCDFileASCII(filename, cloud);
+	pcl::io::savePLYFile("C:/Users/Hp/Desktop/sahne_ply.ply",cloud);
+	std::cerr << "Saved " << cloud.points.size() << " data points to sahne.ply." << std::endl;
+	for (std::size_t i = 0; i < cloud.points.size(); ++i)
+	std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
+	
+	//delete renderColor;
+	delete renderDepth;
+	// close the last opened streams and release any session and processing module instances
+	psm->Release();
+	return 0;
+}
+	
---
+++ Source2.cpp
@@ -0,0 +1,203 @@
+﻿#include <vector>
+#include <thread>
+#include <pcl/features/moment_of_inertia_estimation.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+//#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+//#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/common/time.h>
+#include <iostream>
+#include <sstream>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/dijkstra_shortest_paths.hpp>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char** argv)
+{
+	
+	// Read in the cloud data
+	pcl::PCDReader reader;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>), cloud_f(new pcl::PointCloud<pcl::PointXYZ>);
+	reader.read("D:/Hp/datas/newsahne_pcd.pcd", *cloud);
+	std::cout << "PointCloud before filtering has: " << cloud->points.size() << " data points." << std::endl; //*
+
+																											  // Create the filtering object: downsample the dataset using a leaf size of 50m
+	pcl::VoxelGrid<pcl::PointXYZ> vg;
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
+	vg.setInputCloud(cloud);
+	vg.setLeafSize(10.0f, 10.0f, 10.0f);
+	vg.filter(*cloud_filtered);
+	std::cout << "PointCloud after filtering has: " << cloud_filtered->points.size() << " data points." << std::endl; //*
+
+																													  // Create the segmentation object for the planar model and set all the parameters
+	pcl::SACSegmentation<pcl::PointXYZ> seg;
+	pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+	pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_plane(new pcl::PointCloud<pcl::PointXYZ>());
+	pcl::PCDWriter writer;
+	seg.setOptimizeCoefficients(true);
+	seg.setModelType(pcl::SACMODEL_PLANE);
+	seg.setMethodType(pcl::SAC_RANSAC);
+	seg.setMaxIterations(1000);
+	seg.setDistanceThreshold(50.0);
+	int max_point = 0;
+	int i = 0, nr_points = (int)cloud_filtered->points.size();
+
+	while (cloud_filtered->points.size() > 0.3 * nr_points)
+	{
+		// Segment the largest planar component from the remaining cloud
+		seg.setInputCloud(cloud_filtered);
+		seg.segment(*inliers, *coefficients);
+		if (inliers->indices.size() == 0)
+		{
+			std::cout << "Could not estimate a planar model for the given dataset." << std::endl;
+			break;
+		}
+
+		// Extract the planar inliers from the input cloud
+		pcl::ExtractIndices<pcl::PointXYZ> extract;
+		extract.setInputCloud(cloud_filtered);
+		extract.setIndices(inliers);
+		extract.setNegative(false);
+
+		// Get the points associated with the planar surface
+		extract.filter(*cloud_plane);
+		std::cout << "PointCloud representing the planar component: " << cloud_plane->points.size() << " data points." << std::endl;
+
+		// Remove the planar inliers, extract the rest
+		extract.setNegative(true);
+		extract.filter(*cloud_f);
+		*cloud_filtered = *cloud_f;
+	}
+	
+	//writer.write("C:/Users/Hp/Desktop/groundplane50.pcd", *cloud_plane, false);
+
+	//					 C L U S T E R I N G
+
+	// Creating the KdTree object for the search method of the extraction
+	pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>);
+	tree->setInputCloud(cloud_filtered);
+
+	std::vector<pcl::PointIndices> cluster_indices;
+
+
+	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
+	ec.setClusterTolerance(25.0f); // 2cm
+	ec.setMinClusterSize(100);
+	ec.setMaxClusterSize(25000);
+	ec.setSearchMethod(tree);
+	ec.setInputCloud(cloud_filtered);
+	ec.extract(cluster_indices);
+
+	//std::cout << "1st Cluster: " << cluster_indices[0] << std::endl;
+
+
+	int j = 0;
+	for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it)
+	{
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
+		for (std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); ++pit)
+			cloud_cluster->points.push_back(cloud_filtered->points[*pit]); //*
+		cloud_cluster->width = cloud_cluster->points.size();
+		cloud_cluster->height = 1;
+		cloud_cluster->is_dense = true;
+
+		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size() << " data points." << std::endl;
+		std::stringstream ss;
+		ss << "D:/Hp/objects/deneme/cloud_cluster_" << j << ".pcd";
+		writer.write<pcl::PointXYZ>(ss.str(), *cloud_cluster, false); //*
+		j++;
+	}
+
+	//                   B O U N D I N G         B O X
+	pcl::PointCloud<pcl::PointXYZ>::Ptr bb_cloud(new pcl::PointCloud<pcl::PointXYZ>());
+	//pcl::PCDReader reader;
+	std::ofstream myfile;
+	myfile.open("D:/Hp/myfile2.txt");
+
+	for (int k = 0; k < 5; k++) {
+		std::stringstream clusterFilePath;
+
+		clusterFilePath << "D:/Hp/objects/deneme/cloud_cluster_" << k << ".pcd";
+		
+		reader.read(clusterFilePath.str(), *bb_cloud);
+
+		pcl::MomentOfInertiaEstimation <pcl::PointXYZ> feature_extractor;
+		feature_extractor.setInputCloud(bb_cloud);
+		feature_extractor.compute();
+
+		std::vector <float> moment_of_inertia;
+		std::vector <float> eccentricity;
+		pcl::PointXYZ min_point_AABB;
+		pcl::PointXYZ max_point_AABB;
+		//pcl::PointXYZ min_point_OBB;
+		//pcl::PointXYZ max_point_OBB;
+		//pcl::PointXYZ position_OBB;
+		//Eigen::Matrix3f rotational_matrix_OBB;
+		//float major_value, middle_value, minor_value;
+		//Eigen::Vector3f major_vector, middle_vector, minor_vector;
+		//Eigen::Vector3f mass_center;
+
+		feature_extractor.getMomentOfInertia(moment_of_inertia);
+		feature_extractor.getEccentricity(eccentricity);
+		feature_extractor.getAABB(min_point_AABB, max_point_AABB);
+		std::cout << "min points" << min_point_AABB.x << std::endl;
+		std::cout << "max points" << max_point_AABB.x << std::endl;
+
+		if (myfile.is_open()) {
+			myfile << min_point_AABB.x << " " << min_point_AABB.y << " " << min_point_AABB.z << "\n" << max_point_AABB.x << " " << max_point_AABB.y << " " << max_point_AABB.z << "\n";
+		}
+		
+	}
+	myfile.close();
+	
+	
+
+	//feature_extractor.getOBB(min_point_OBB, max_point_OBB, position_OBB, rotational_matrix_OBB);
+
+	//feature_extractor.getEigenValues(major_value, middle_value, minor_value);
+	//feature_extractor.getEigenVectors(major_vector, middle_vector, minor_vector);
+	//feature_extractor.getMassCenter(mass_center);
+
+	//pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+	//viewer->setBackgroundColor(0, 0, 0);
+	//viewer->addCoordinateSystem(1.0);
+	//viewer->initCameraParameters();
+	//viewer->addPointCloud<pcl::PointXYZ>(cloud, "sample cloud");
+	//viewer->addCube(min_point_AABB.x, max_point_AABB.x, min_point_AABB.y, max_point_AABB.y, min_point_AABB.z, max_point_AABB.z, 1.0, 1.0, 0.0, "AABB");
+	//viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "AABB");
+
+	//Eigen::Vector3f position(position_OBB.x, position_OBB.y, position_OBB.z);
+	//Eigen::Quaternionf quat(rotational_matrix_OBB);
+	//viewer->addCube(position, quat, max_point_OBB.x - min_point_OBB.x, max_point_OBB.y - min_point_OBB.y, max_point_OBB.z - min_point_OBB.z, "OBB");
+	//viewer->setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_REPRESENTATION, pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "OBB");
+
+	//pcl::PointXYZ center(mass_center(0), mass_center(1), mass_center(2));
+	//pcl::PointXYZ x_axis(major_vector(0) + mass_center(0), major_vector(1) + mass_center(1), major_vector(2) + mass_center(2));
+	//pcl::PointXYZ y_axis(middle_vector(0) + mass_center(0), middle_vector(1) + mass_center(1), middle_vector(2) + mass_center(2));
+	//pcl::PointXYZ z_axis(minor_vector(0) + mass_center(0), minor_vector(1) + mass_center(1), minor_vector(2) + mass_center(2));
+	//viewer->addLine(center, x_axis, 1.0f, 0.0f, 0.0f, "major eigen vector");
+	//viewer->addLine(center, y_axis, 0.0f, 1.0f, 0.0f, "middle eigen vector");
+	//viewer->addLine(center, z_axis, 0.0f, 0.0f, 1.0f, "minor eigen vector");
+	/*
+	while (!viewer->wasStopped())
+	{
+		viewer->spinOnce(100);
+		std::this_thread::sleep_for(100ms);
+	}*/
+
+
+	return (0);
+}
+	
